cdbe96695ac056f7e83251c7fcdff6104225f48b
Stranger488 <ilya_goltsov@list.ru>
2020-02-23 14:37:19 +0300

    after check, commit of forgotten main.cpp

--- main.cpp
+++ main.cpp
@@ -46,12 +46,31 @@ class StringFieldsHolder {
 private:
     std::vector<StringField> strings;
 public:
+    void drawString(const size_t& index) const {
+        strings.at(index).drawWords();
+        std::cout << std::endl;
+    }
+
+    bool drawStrings() const {
+        try {
+            for (const auto &string_field : strings) {
+                string_field.drawWords();
+                std::cout << std::endl;
+            }
+        } catch (const std::exception& e) {
+            std::cout << e.what() << std::endl;
+            return false;
+        }
+
+        return true;
+    }
+
     bool readStrings() {
         std::string buffer_line;
 
         while (!std::cin.eof()) {
-            std::getline(std::cin, buffer_line);
             StringField tmp;
+            std::getline(std::cin, buffer_line);
             tmp.readLine(buffer_line);
             strings.push_back(tmp);
         }
@@ -61,44 +80,45 @@ public:
 
     bool addWordToField(const size_t& indexField, const std::string& word) {
         strings.at(indexField).addWord(word);
-
+        drawString(indexField);
 
         return true;
     }
 
     bool editWordInField(const size_t& indexWord, const size_t & indexField, const std::string& new_value) {
         strings.at(indexField).editWord(indexWord, new_value);
+        drawString(indexField);
 
         return true;
     };
 
     bool deleteWordInField(const size_t& indexWord, const size_t& indexField) {
         strings.at(indexField).deleteWord(indexWord);
+        drawString(indexField);
 
         return true;
-    }
-
-    void drawString(const size_t& index) {
-        strings.at(index).drawWords();
-    }
-
-    void drawStrings() {
-        for (const auto& string_field : strings) {
-            string_field.drawWords();
-            std::cout << '\n';
-        }
     }
 };
 
 int main() {
     StringFieldsHolder stringFieldsHolder;
-    stringFieldsHolder.readStrings();
 
-    stringFieldsHolder.addWordToField(0, "newField");
-    stringFieldsHolder.editWordInField(0, 0, "editedField");
-    stringFieldsHolder.deleteWordInField(0, 0);
+    try {
+        stringFieldsHolder.readStrings();
 
-    stringFieldsHolder.drawStrings();
+//        stringFieldsHolder.addWordToField(1, "newField");
+//        stringFieldsHolder.addWordToField(0, "newField2");
+//
+//        stringFieldsHolder.editWordInField(0, 0, "editedField");
+//
+//        stringFieldsHolder.deleteWordInField(0, 1);
+//        stringFieldsHolder.deleteWordInField(1, 1);
+//
+//        stringFieldsHolder.drawStrings();
+    } catch (const std::exception& e) { // basic exception handling
+        std::cout << e.what() << std::endl;
+        return -1;
+    }
 
     return 0;
 }
